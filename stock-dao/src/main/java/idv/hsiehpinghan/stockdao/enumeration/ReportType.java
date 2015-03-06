1f8f98320f379a10cc022e812be10217a59386a6
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2015-03-06 15:41:19 +0800

    Backup.

--- stock-dao/src/main/java/idv/hsiehpinghan/stockdao/enumeration/ReportType.java
+++ stock-dao/src/main/java/idv/hsiehpinghan/stockdao/enumeration/ReportType.java
@@ -1,7 +1,34 @@
 package idv.hsiehpinghan.stockdao.enumeration;
 
+import java.util.Locale;
+
 public enum ReportType {
-	CONSOLIDATED_STATEMENT, INDIVIDUAL_STATEMENT, ENTERPRISE_STATEMENT;
+	CONSOLIDATED_STATEMENT("合併", "consolidated"), INDIVIDUAL_STATEMENT(
+			"個別", "individual"), ENTERPRISE_STATEMENT("個體",
+			"enterprise");
+	private String chineseName;
+	private String englishName;
+
+	private ReportType(String chineseName, String englishName) {
+		this.chineseName = chineseName;
+		this.englishName = englishName;
+	}
+
+	public String getChineseName() {
+		return this.chineseName;
+	}
+
+	public String getEnglishName() {
+		return this.englishName;
+	}
+
+	public String getName(Locale locale) {
+		if (Locale.ENGLISH.getLanguage().equals(locale.getLanguage())) {
+			return getEnglishName();
+		} else {
+			return getChineseName();
+		}
+	}
 
 	public static ReportType getMopsReportType(String code) {
 		switch (code) {
